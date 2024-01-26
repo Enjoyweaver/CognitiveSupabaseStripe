4e374f6f877708d9ab29fbd023e1dfdbe0791ecc
Michael <enjoyweaver@gmail.com>
2024-01-26 10:45:15 -0600

    main

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,12 @@ import { PropsWithChildren } from 'react';
 import 'styles/main.css';
 
 const meta = {
-  title: 'Next.js Subscription Starter',
-  description: 'Brought to you by Vercel, Stripe, and Supabase.',
+  title: 'Cognitive labs',
+  description: 'Schedule your day with ease',
   cardImage: '/og.png',
   robots: 'follow, index',
   favicon: '/favicon.ico',
-  url: 'https://subscription-starter.vercel.app',
+  url: 'https://cognitivelabs.vercel.app',
   type: 'website'
 };
 
