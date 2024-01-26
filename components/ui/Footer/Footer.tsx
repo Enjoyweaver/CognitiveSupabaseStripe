117f893b99e45ae0355241c5fdc5ff84234048dc
Michael <enjoyweaver@gmail.com>
2024-01-26 12:09:43 -0600

    main

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -90,23 +90,6 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-between py-12 space-y-4 md:flex-row bg-zinc-900">
-        <div>
-          <span>
-            &copy; {new Date().getFullYear()} ACME, Inc. All rights reserved.
-          </span>
-        </div>
-        <div className="flex items-center">
-          <span className="text-white">Crafted by</span>
-          <a href="https://vercel.com" aria-label="Vercel.com Link">
-            <img
-              src="/vercel.svg"
-              alt="Vercel.com Logo"
-              className="inline-block h-6 ml-4 text-white"
-            />
-          </a>
-        </div>
-      </div>
     </footer>
   );
 }
